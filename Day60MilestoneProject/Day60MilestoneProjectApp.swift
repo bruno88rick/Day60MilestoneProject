95e537aeb9f0ca3e269b402caeefbe1951e7c547
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-07-03 14:32:14 -0300

    Initial Commit

---
+++ Day60MilestoneProject/Day60MilestoneProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day60MilestoneProjectApp.swift
+//  Day60MilestoneProject
+//
+//  Created by Bruno Oliveira on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Day60MilestoneProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
